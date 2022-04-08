5740b353e3ed9cfae2efa4d3f57ff1217a0bf495
DJ-Dav <davyj713@gmail.com>
2022-04-08 19:18:20 +0100

    Add project files.

---
+++ Player.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public class Player : KinematicBody2D
+{
+    [Export] public float Speed = 300;
+    [Export] public float Jump = 300;
+    [Export] public float Gravity = -100;
+
+    Vector2 velocity = Vector2.Zero;
+
+    public override void _Ready()
+    {
+        
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        float input = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
+        input = Mathf.Clamp(input, -1f, 1f);
+
+    }
+}
